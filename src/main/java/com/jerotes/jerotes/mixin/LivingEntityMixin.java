71acfcca866d9be2a8879138e709b2ec5f0fc7ef
Jerotes <2694429769@qq.com>
2026-02-17 08:45:29 +0800

    init project

--- src/main/java/com/jerotes/jerotes/mixin/LivingEntityMixin.java
+++ src/main/java/com/jerotes/jerotes/mixin/LivingEntityMixin.java
@@ -30,6 +30,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -98,6 +99,7 @@ public abstract class LivingEntityMixin extends Entity implements JerotesChangeL
         return this.canAddPassenger(entity);
     }
 
+    @Unique
     @Nullable
     protected Object2LongMap<Entity> recentKineticEnemiesJerotes;
     public boolean wasRecentlyStabbedJerotes(Entity entity, int n) {
